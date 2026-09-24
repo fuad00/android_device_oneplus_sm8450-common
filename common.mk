45994b2cc8585acc152e12523f898f678225923f
fuad00 <fuad00@users.noreply.github.com>
2026-09-25 01:22:19 +0300

    sm8450-common: Set USE_EFFECT_STREAM as a bool soong config variable
    hardware/oplus selects on it with bool keys, which lineage-23.2 soong
    type-checks.

--- common.mk
+++ common.mk
@@ -347,7 +347,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-$(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
+$(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oplus
