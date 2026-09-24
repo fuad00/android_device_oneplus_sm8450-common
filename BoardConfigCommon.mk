e2350a7075826ac10c3937ce5e0515c5e924a2dd
fuad00 <fuad00@users.noreply.github.com>
2026-09-25 01:16:10 +0300

    sm8450-common: Resolve leftover merge conflict in BoardConfigCommon.mk
    The lineage-23.2 rebase left conflict markers around the HIDL block.
    The block now lives in common.mk (vintf/ paths, recategorized flags,
    framework_manifest.xml included), so take the HEAD side and drop it
    from BoardConfigCommon.mk.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,18 +56,6 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
-<<<<<<< HEAD
-=======
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    hardware/oplus/vintf/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
-
->>>>>>> 4f56b32 (sm8450-common: Import QCC system_ext blobs)
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_BOOTCONFIG:= \
